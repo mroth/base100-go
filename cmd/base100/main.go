3deda3e57a325f51e8cb750e8f937adfe7a9faf9
Matthew Rothenberg <mrothenberg@gmail.com>
2020-05-22 11:47:27 -0400

    clean up CLI flags

--- cmd/base100/main.go
+++ cmd/base100/main.go
@@ -16,24 +16,52 @@ const (
 	productFullName  = "base💯 (Go)"
 )
 
-var (
-	decode = flag.Bool("decode", false, "decode data")
-)
+type options struct {
+	decode        bool   // decode input instead of encode
+	input, output string // optional file paths
+}
+
+func cliParse() (opts options) {
+	flag.Usage = func() {
+		fmt.Fprintf(flag.CommandLine.Output(), `%s
+Encodes things into emoji
+
+USAGE:
+    base100 [FLAGS]
+
+FLAGS:
+    -d, --decode     Decodes input
+    -i, --input      Input file (default use STDIN)
+    -o, --output     Output file (default use STDOUT)
+    -h, --help       Prints help information
+`, productFullName)
+	}
+
+	flag.BoolVar(&opts.decode, "decode", false, "")
+	flag.BoolVar(&opts.decode, "d", false, "")
+	flag.StringVar(&opts.input, "input", "", "")
+	flag.StringVar(&opts.input, "i", "", "")
+	flag.StringVar(&opts.output, "output", "", "")
+	flag.StringVar(&opts.output, "o", "", "")
+
+	flag.Parse()
+	return
+}
 
 func main() {
-	flag.Parse()
+	opts := cliParse()
 
-	in, out := os.Stdin, os.Stdout
-	if flag.Arg(0) != "" {
-		log.Fatal("not handling <INPUT> yet...")
+	if opts.input != "" || opts.output != "" {
+		log.Fatal("TODO: not handling files just yet...")
 	}
+	in, out := os.Stdin, os.Stdout
 	defer out.Close()
 
 	reader := bufio.NewReader(in)
 	writer := bufio.NewWriter(out)
 	defer writer.Flush()
 
-	if *decode {
+	if opts.decode {
 		// decoder currently dies due to lack of CRLF filtering
 		decoder := base100.NewDecoder(reader)
 		_, err := io.Copy(writer, decoder)
